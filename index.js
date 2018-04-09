38a5ec5d3cfc4d638dfbc9e5a4d955da3d9fcba1
Billi Ramirez <ramirezbilly42@outlook.com>
2018-04-08 22:29:13 -0600

    Starting the project

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress,graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+export const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+
+
+const myGraphQLSchema = schema; // ... define or import your schema here!
+const PORT = 8080;
+
+const app = express();
+const graphqlEndpoint = '/grapqhl'
+
+// bodyParser is needed just for POST.
+app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
+
+
+app.listen(PORT);
